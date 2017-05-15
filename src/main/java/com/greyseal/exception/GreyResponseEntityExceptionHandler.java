7bcc7e1d3181fd4b5fe3a5e474bb7bf192570761
SaurabhK <saubkukreti@gmail.com>
2017-05-15 15:41:00 +0530

    added exception handler for MethodNotAllowed

--- src/main/java/com/greyseal/exception/GreyResponseEntityExceptionHandler.java
+++ src/main/java/com/greyseal/exception/GreyResponseEntityExceptionHandler.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.greyseal.patterns.ErrorBuilder;
@@ -22,12 +25,21 @@ public class GreyResponseEntityExceptionHandler extends ResponseEntityExceptionH
 
 	@ExceptionHandler(value = { GreyException.class })
 	public ResponseEntity<ResponseBuilder<ErrorBuilder>> handleUserNotFoundException(final GreyException ex, final HttpServletRequest request) {
+		return new ResponseEntity<ResponseBuilder<ErrorBuilder>>(buildResponse(ex), new HttpHeaders(), ex.getStatus());
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
+		GreyException gx = new GreyException(status.value(), status, status.getReasonPhrase());
+		return new ResponseEntity<Object>(buildResponse(gx), new HttpHeaders(), status );
+	}
+	
+	protected ResponseBuilder<ErrorBuilder> buildResponse (final GreyException ex){
 		ErrorBuilder error = new ErrorBuilder.Builder()
-											 .setStatus(ex.getCode())
-											 .setMessage(ex.getMessage())
-											 .setTimeStamp(new Date().toString())
-											 .build();
-		ResponseBuilder<ErrorBuilder> response = ResponseBuilder.build(error, ex.getCode(), ex.getMessage());
-		return new ResponseEntity<ResponseBuilder<ErrorBuilder>>(response, new HttpHeaders(), ex.getStatus());
+				 							.setStatus(ex.getCode())
+			 								.setMessage(ex.getMessage())
+			 								.setTimeStamp(new Date().toString())
+			 								.build();
+		return ResponseBuilder.build(error, ex.getCode(), ex.getMessage());
 	}
 }
